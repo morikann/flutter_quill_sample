6bd3f0283d74f868dd2d6b90e494169fcc90f31b
morikann <mrknt0088@gmail.com>
2023-06-08 11:23:37 +0900

    show Quill

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_quill/flutter_quill.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final QuillController _controller = QuillController.basic();
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: SafeArea(
+          child: Column(
+            children: [
+              QuillToolbar.basic(controller: _controller),
+              Expanded(
+                // Expandedだけだとレイアウトエラーが起こるのでLayoutBuilderを利用
+                child: LayoutBuilder(
+                  builder: (context, constraints) {
+                    return QuillEditor.basic(
+                      controller: _controller,
+                      readOnly: false,
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
